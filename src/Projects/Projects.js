26d3daaddcf869c1e1a2049eb2b90d398b7faedb
NishantSinghhhhh <nishantsingh_230137@aitpune.edu.in>
2024-08-31 19:18:43 +0530

    changed the projects page

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>July 2024</p>
                             </div>
-                            <a href="/">
+                            <a target='_blank' href="https://yoooom.netlify.app/">
                                 <div className="project-page-item-header">
                                     <h1>Yoom</h1>
                                     <p>-NEXTJS PRISMA-orm </p>
@@ -38,13 +38,13 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>May 2024 June 2024</p>
                             </div>
-                            <a href="/">
+                            <a target="_blank"  href="https://ai-content-generator-01.netlify.app/">
                                 <div className="project-page-item-header">
                                     <h1>AI Content Generator</h1>
                                     <p>Nextjs gemini</p>
                                 </div>
                                 <div className="project-page-item-image project-page-item-image2"></div>
-                                <a href="https://ai-content-generator-01.netlify.app/" className="project-page-item-footer" target="_blank" rel="noopener noreferrer">
+                                <a target="_blank" href="https://ai-content-generator-01.netlify.app/" className="project-page-item-footer" target="_blank" rel="noopener noreferrer">
                                 <p>Live Link</p>
                                     <img src={arrow} alt="Arrow" />
                                 </a>
@@ -55,13 +55,13 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>Februrary 2024 </p>
                             </div>
-                            <a href="/">
+                            <a target="_blank" href="https://virtual-ir-ten.vercel.app/">
                                 <div className="project-page-item-header">
                                     <h1>Virtual IR</h1>
                                     <p></p>
                                 </div>
                                 <div className="project-page-item-image project-page-item-image3"></div>
-                                <a href="https://virtual-ir-ten.vercel.app/" className="project-page-item-footer" target="_blank" rel="noopener noreferrer">
+                                <a target="_blank" href="https://virtual-ir-ten.vercel.app/" className="project-page-item-footer" target="_blank" rel="noopener noreferrer">
                                 <p>Live Link</p>
                                     <img src={arrow} alt="Arrow" />
                                 </a>
@@ -71,7 +71,7 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>February 2024</p>
                             </div>
-                            <a href="/">
+                            <a target='_blank' href="https://dashboard-three-delta-61.vercel.app/">
                                 <div className="project-page-item-header">
                                     <h1>Dashboard </h1>
                                     <p>Reactjs tailwind Css </p>
@@ -87,7 +87,7 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>August 2024</p>
                             </div>
-                            <a href="/">
+                            <a  target="_blank" href="https://spark2k24-25.netlify.app/">
                                 <div className="project-page-item-header">
                                     <h1>Spark 2k24-2k24</h1>
                                     <p>Html css & Javascript</p>
@@ -103,7 +103,7 @@ function Projects() {
                             <div className="project-page-item-number">
                                 <p>December 2023</p>
                             </div>
-                            <a href="/">
+                            <a href="https://candid-nasturtium-1b0197.netlify.app/" target='_blank'>
                                 <div className="project-page-item-header">
                                     <h1>Saif's Cuisine</h1>
                                     <p>HTMl & css</p>
